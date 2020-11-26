20c2ca1437342822d6f53fb48d2724d9d35000f2
Edson Ayllon <eayllon1@gmail.com>
2020-11-26 00:23:58 -0500

    fix(type): Fix and add contract type

--- app/src/config/configureStore.ts
+++ app/src/config/configureStore.ts
@@ -2,15 +2,17 @@ import Web3 from 'web3';
 import { initialState as state } from 'GlobalState'
 import { StateInterface } from 'types/state.types'
 import { SYNTH_ADDRESS } from 'utils/const'
+import { getAbi } from 'utils/fn'
 import ERC20 from 'contracts/ERC20.json'
 
 // configure initial state
 const configureStore = async () => {
     const web3 = new Web3(Web3.givenProvider || 'https://api.mycryptoapi.com/eth')
-    // const sUSDContract = new web3.eth.Contract(ERC20.abi, SYNTH_ADDRESS.mainnet.SUSD)
+    const sUSDContract = new web3.eth.Contract(getAbi(ERC20), SYNTH_ADDRESS.mainnet.SUSD)
     const initialState: StateInterface = {
       ...state,
-      web3
+      web3,
+      sUSDContract
     }
     console.log('✓ Created Store')
     return { initialState }
--- app/src/types/state.types.ts
+++ app/src/types/state.types.ts
@@ -1,7 +1,9 @@
-import { EmitHelper } from 'typescript';
 import Web3 from 'web3';
+import { Contract } from "web3-eth-contract";
 
 export type StateInterface = {
     activeStep?: number,
-    web3?: Web3
+    web3?: Web3,
+    sUSDContract?: Contract,
+    poolContract?: Contract
 }
