0791aff8606b60f5448c9fc079b1a297614a999a
Edson Ayllon <eayllon1@gmail.com>
2021-03-23 21:41:32 -0400

    feat(CreatePool): Add create pool functionality

--- src/components/manager/SelectFund.tsx
+++ src/components/manager/SelectFund.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Button } from "@gnosis.pm/safe-react-components"
+import { Button, Title, Text } from "@gnosis.pm/safe-react-components"
 
 import { GlobalState } from 'GlobalState'
 import { PoolSelectionInput } from "components/forms"
@@ -8,18 +8,14 @@ import { DividerWithText } from "components/ui"
 const SelectFund: React.FC = () => {
     const [state, setState] = useContext(GlobalState)
 
-    const confirmSelection = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>,contractAddress: string) => {
-        e.preventDefault();
-        setState({ ...state, activeStep: 1, poolContractAddress: contractAddress });
-    }
-
     const handleCreatePool = () => {
         setState({ ...state, activeStep: 1, createPool: true });
     }
 
     return (
         <div className = "padding-16">
-            <PoolSelectionInput confirmSelection={confirmSelection} />
+            <Title size="sm">Manage Existing Pool</Title>
+            <Text size="lg">Coming soon...</Text>
             <DividerWithText label="Or" />
             <Button size = "md" color = "primary" onClick={handleCreatePool}>
                 Create a New Pool
